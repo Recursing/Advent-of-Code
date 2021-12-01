30bb1421f773893fb5265c99a7f59cf2ec64a025
Recursing <buonanno.lorenzo@gmail.com>
2021-12-02 00:49:37 +0100

    Add count util To allow `arr.filter(condition).length` instead of `sum(arr.map(condition))`

--- Advent-of-Code-2021/day01.ts
+++ Advent-of-Code-2021/day01.ts
@@ -2,6 +2,11 @@ import * as utils from "./utils.ts";
 
 const nums = (await utils.readDay(1)).map((line) => parseInt(line, 10));
 let pairs = utils.zip(nums, nums.slice(1));
-console.log(utils.sum(pairs.map(([a, b]) => a < b)));
+console.log(utils.count(pairs, ([a, b]) => a < b));
 pairs = utils.zip(nums, nums.slice(3));
-console.log(utils.sum(pairs.map(([a, b]) => a < b)));
+console.log(utils.count(pairs, ([a, b]) => a < b));
+
+// More idiomatic implementation, comparison with undefined is always false
+// so no need to do `value > (nums[index - 1] ?? Infinity)`
+console.log(utils.count(nums, (value, index) => value > nums[index - 1]));
+console.log(utils.count(nums, (value, index) => value > nums[index - 3]));
--- Advent-of-Code-2021/utils.ts
+++ Advent-of-Code-2021/utils.ts
@@ -1,16 +1,26 @@
 // built-ins I miss from python
 // don't want to import lodash but maybe I should, it has everything here
 
-export function sum(array: (number | boolean)[]): number {
-  return array.reduce<number>((a, b) => a + +b, 0);
+export function sum(array: number[]): number {
+  return array.reduce<number>((a, b) => a + b, 0);
+}
+
+export function count<T>(
+  array: Array<T>,
+  condition: (arg0: T, index: number) => boolean
+): number {
+  return array.filter(condition).length;
 }
 
 export function range(end: number): number[] {
   return [...Array(end).keys()];
 }
 
-export function zip<T>(...args: T[][]): T[][] {
+export function zip<T extends unknown[][]>(
+  ...args: T
+): { [K in keyof T]: T[K] extends (infer V)[] ? V : never }[] {
   const minLength = Math.min(...args.map((arr) => arr.length));
+  // @ts-expect-error This is too much for ts
   return range(minLength).map((i) => args.map((arr) => arr[i]));
 }
 
