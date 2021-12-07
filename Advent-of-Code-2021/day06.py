eece20c01d4c9a045c993616a1655898b5886cbd
Recursing <buonanno.lorenzo@gmail.com>
2021-12-07 16:13:58 +0100

    Day 6 alternative solution

--- Advent-of-Code-2021/day06.py
+++ Advent-of-Code-2021/day06.py
@@ -1,3 +1,4 @@
+from collections import deque
 from functools import cache
 
 timers = list(map(int, open("day06.txt").read().split(",")))
@@ -13,3 +14,15 @@ def offspring_size(first_due: int, day: int) -> int:
 
 print(sum(offspring_size(first_due, 80) for first_due in timers))
 print(sum(offspring_size(first_due, 256) for first_due in timers))
+
+
+def alternative_solution(nums: list[int], days: int = 256) -> int:
+    fish_groups = deque(nums.count(i) for i in range(9))
+    for _ in range(days):
+        fish_groups[7] += fish_groups[0]
+        fish_groups.rotate(-1)
+    return sum(fish_groups)
+
+
+assert alternative_solution(timers, 80) == 350149
+assert alternative_solution(timers, 256) == 1590327954513
